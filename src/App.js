3a87e890c8dd0d868a46fdc0187182ad1026c749
Nishimura <yukinkopen@gmail.com>
2021-07-17 04:42:22 +0900

    [mod] ＋ボタン押下時にアイテムを追加する動作を実行

--- src/App.js
+++ src/App.js
@@ -1,38 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+const list = []
+const numlist = [1, 2, 3]
 
 function App() {
-  const [text, setText] = useState('')
+  const [item, setItem] = useState({ text: '', isComplete: false })
+  const [dummy, setDummy] = useState({ text: '', isComplete: false })
+
   const handleChange = (e) => {
-    setText(() => e.target.value)
+    setItem({ ...item, text: e.target.value })
+    console.log('レンダリング handleChange')
   }
-  const onClickRegisterBtn = () => {
-    console.log(text)
+
+  const onClickRegisterBtn = (e) => {
+    e.preventDefault()
+    let newItem = { ...item }
+    setItem(newItem)
+    list.push(item)
+    console.log(list)
+    console.log('レンダリング onClickRegisterBtn')
+    e.target.value = ''
   }
+
+  console.log('レンダリングApp')
 
   return (
     <div className="App">
+      {console.log('レンダリングHTML')}
       <div>
         <div className="text-center">
           <h1 className="text-center text-3xl p-3 text-white bg-blue-800">
             ToDoアプリ
           </h1>
-          <input
-            value={text}
-            onChange={handleChange}
-            className="m-2 p-2 border border-blue-500 w-80"
-            type="text"
-            placeholder="例：買い物に行く"
-          />
-          <button
-            onClick={onClickRegisterBtn}
-            className="bg-blue-200 py-2 px-4 text-blue-500"
-          >
-            ✙
-          </button>
+          <form>
+            <input
+              onChange={handleChange}
+              value={item.text}
+              className="m-2 p-2 border border-blue-500 w-80"
+              type="text"
+              placeholder="例：買い物に行く"
+            />
+            <button
+              onClick={onClickRegisterBtn}
+              className="bg-blue-200 py-2 px-4 text-blue-500"
+            >
+              ✙
+            </button>
+          </form>
         </div>
         <div>
           <h2 className="text-2xl m-2 p-2 text-blue-800">未完了</h2>
           <ul className="m-4 text-center">
+            {console.log('レンダリングHTML2 size=' + list.length)}
+            {/* {list[0]?.text} */}
+            {list.map((item, index) => (
+              // console.log('レンダリングHTML3')
+              <li key={index}>{item.text}</li>
+            ))}
             <li className="w-1/2 p-2 mr-2 inline-block border">
               <div className="flex justify-evenly">
                 <span className="text-center w-3/4">掃除をする</span>
