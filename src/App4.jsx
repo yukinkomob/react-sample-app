e10a22f4ed598e48ba5a5a8341d3c6cc047659e9
Nishimura <yukinkopen@gmail.com>
2021-07-23 05:16:05 +0900

    [mod] カレンダーをサンプル表示

--- src/App4.jsx
+++ src/App4.jsx
@@ -1,4 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
+import DayPicker from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+
+const birthdayStyle = `.DayPicker-Day--highlighted {
+  background-color: orange;
+  color: white;
+}`
+
+const modifiers = {
+  highlighted: new Date(2018, 8, 19),
+}
+
 export default function App4() {
-  return <div>App4</div>
+  return (
+    <div>
+      <style>{birthdayStyle}</style>
+      <DayPicker modifiers={modifiers} month={new Date(2018, 8)} />
+    </div>
+  )
 }
