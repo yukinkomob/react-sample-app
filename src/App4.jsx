9159c9f6720886077f61d8f9315c02740134b5aa
Nishimura <yukinkopen@gmail.com>
2021-07-23 05:47:11 +0900

    [mod] ダイアログにスタイルを適用

--- src/App4.jsx
+++ src/App4.jsx
@@ -6,21 +6,38 @@ function App4() {
   let [isOpen, setIsOpen] = useState(true)
 
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+    <Dialog
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+      className="bg-gray-100 border border-blue-500 p-4 w-96"
+    >
       <Dialog.Overlay />
 
-      <Dialog.Title>Deactivate account</Dialog.Title>
-      <Dialog.Description>
+      <Dialog.Title className="text-3xl p-4 font-bold">
+        Deactivate account
+      </Dialog.Title>
+      <Dialog.Description className="text-xl p-2">
         This will permanently deactivate your account
       </Dialog.Description>
 
-      <p>
+      <p className="p-2">
         Are you sure you want to deactivate your account? All of your data will
         be permanently removed. This action cannot be undone.
       </p>
-
-      <button onClick={() => setIsOpen(false)}>Deactivate</button>
-      <button onClick={() => setIsOpen(false)}>Cancel</button>
+      <div className="text-center space-x-10 p-4">
+        <button
+          className="rounded-lg bg-blue-500 p-3 text-white"
+          onClick={() => setIsOpen(false)}
+        >
+          Deactivate
+        </button>
+        <button
+          className="rounded-lg bg-blue-500 p-3 text-white"
+          onClick={() => setIsOpen(false)}
+        >
+          Cancel
+        </button>
+      </div>
     </Dialog>
   )
 }
