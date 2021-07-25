3815ef2997ce656327156f6099ad1796137f577c
Nishimura <yukinkopen@gmail.com>
2021-07-25 19:55:55 +0900

    [mod] 詳細画面を作成   - サンプルデータを設定

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,24 +1,70 @@
 import React from 'react'
 import { useParams, useLocation, Link } from 'react-router-dom'
+import Header from '../components/Header'
 
 const Detail = () => {
   const { id } = useParams()
   const location = useLocation()
-  const test = location.state.test
+  const data = location.state.data
 
   return (
     <div>
-      <p>t:{test}</p>
-      <h2>ID : {id} の内容</h2>
-      <h3>掃除をする</h3>
-      <h3>(カテゴリ)</h3>
-      <p className="w-48 break-words">
-        asdfssssssssssssssssssssssssssssssssssssssdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfaafasdfasdfa
-      </p>
-      <p>期限：</p>
-      <p>登録日：</p>
-      <div>
-        temp message <Link to="/">link-{id}</Link>
+      <Header title="ToDoアプリ" />
+      <h2 className="p-4 text-center text-2xl">ID : {id} の内容</h2>
+      <div className="flex justify-center">
+        <table className="m-4 text-center bg-blue-200">
+          <tr>
+            <td className="p-4 text-xl">Title</td>
+            <td className="p-4">
+              <input
+                className="p-2 text-center text-xl w-full"
+                type="text"
+                value={data.title}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className="p-8 text-xl">カテゴリ</td>
+            <td className="p-4">
+              <input
+                className="p-2 text-center text-xl w-full"
+                type="text"
+                value={data.category}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className="p-8 text-xl">内容</td>
+            <td className="p-4 w-96">
+              <textarea className="p-2 w-full text-left">
+                {data.content}
+              </textarea>
+            </td>
+          </tr>
+          <tr>
+            <td className="p-8 text-xl">期限</td>
+            <td className="p-4">
+              <input
+                className="p-2 text-center text-xl w-full"
+                type="text"
+                value={data.expiredDate}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className="p-8 text-xl">登録日</td>
+            <td className="p-4">
+              <input
+                className="p-2 text-center text-xl w-full"
+                type="text"
+                value={data.registeredDate}
+              />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div className="m-4 text-lg text-blue-800 cursor-pointer">
+        <Link to="/">一覧表示に戻る</Link>
       </div>
     </div>
   )
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -33,11 +33,21 @@ const Main = () => {
     setList([...list])
   }
 
+  const sampleData = {
+    id: 100,
+    title: '掃除をする',
+    category: '家事2',
+    content:
+      '最近溜まっていた部分の掃除をする。掃除には掃除道具を利用して、効率的に進める。２',
+    expiredDate: '2022-07-25',
+    registeredDate: '2022-07-20',
+  }
+
   const goToDetail = (e, id) => {
     e.preventDefault()
     e.stopPropagation()
     console.log('gotodetail' + id)
-    history.push(`/temp/${id}`, { test: 'test123' })
+    history.push(`/temp/${id}`, { data: sampleData })
   }
 
   return (
