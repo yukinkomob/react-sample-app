36722d8fd6a76a71c91f328f508220989e1f0160
Nishimura <yukinkopen@gmail.com>
2021-07-25 20:10:26 +0900

    [mod] warning を除去   - tbodyを追加   - textareaの値をdefaultValueに設定

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -13,54 +13,72 @@ const Detail = () => {
       <h2 className="p-4 text-center text-2xl">ID : {id} の内容</h2>
       <div className="flex justify-center">
         <table className="m-4 text-center bg-blue-200">
-          <tr>
-            <td className="p-4 text-xl">Title</td>
-            <td className="p-4">
-              <input
-                className="p-2 text-center text-xl w-full"
-                type="text"
-                value={data.title}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td className="p-8 text-xl">カテゴリ</td>
-            <td className="p-4">
-              <input
-                className="p-2 text-center text-xl w-full"
-                type="text"
-                value={data.category}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td className="p-8 text-xl">内容</td>
-            <td className="p-4 w-96">
-              <textarea className="p-2 w-full text-left">
-                {data.content}
-              </textarea>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-8 text-xl">期限</td>
-            <td className="p-4">
-              <input
-                className="p-2 text-center text-xl w-full"
-                type="text"
-                value={data.expiredDate}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td className="p-8 text-xl">登録日</td>
-            <td className="p-4">
-              <input
-                className="p-2 text-center text-xl w-full"
-                type="text"
-                value={data.registeredDate}
-              />
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="p-4 text-xl">Title</td>
+              <td className="p-4">
+                <input
+                  className="p-2 text-center text-xl w-full"
+                  type="text"
+                  value={data.title}
+                  onChange={(e) => {
+                    data.title = e.target.value
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="p-8 text-xl">カテゴリ</td>
+              <td className="p-4">
+                <input
+                  className="p-2 text-center text-xl w-full"
+                  type="text"
+                  value={data.category}
+                  onChange={(e) => {
+                    data.category = e.target.value
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="p-8 text-xl">内容</td>
+              <td className="p-4 w-96">
+                <textarea
+                  className="p-2 w-full text-left"
+                  defaultValue={data.content}
+                  onChange={(e) => {
+                    data.content = e.target.value
+                  }}
+                ></textarea>
+              </td>
+            </tr>
+            <tr>
+              <td className="p-8 text-xl">期限</td>
+              <td className="p-4">
+                <input
+                  className="p-2 text-center text-xl w-full"
+                  type="text"
+                  value={data.expiredDate}
+                  onChange={(e) => {
+                    data.expiredDate = e.target.value
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="p-8 text-xl">登録日</td>
+              <td className="p-4">
+                <input
+                  className="p-2 text-center text-xl w-full"
+                  type="text"
+                  value={data.registeredDate}
+                  onChange={(e) => {
+                    data.registeredDate = e.target.value
+                  }}
+                />
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
       <div className="m-4 text-lg text-blue-800 cursor-pointer">
