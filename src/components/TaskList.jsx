41595a6134943c3d9659ce3a878059dbf9ada414
Nishimura <yukinkopen@gmail.com>
2021-07-27 03:33:23 +0900

    [mod] react-beautiful-dndのサンプルコードの一部を移植

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,8 +1,60 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import PropTypes from 'prop-types'
 import TaskItem from './TaskItem'
 import { LocalStorageFuncs, ToastFuncs } from '../pages/Main'
 import { DialogFuncs } from './AlertDialog'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+// fake data generator
+const getItems = (count, offset = 0) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `item-${k + offset}-${new Date().getTime()}`,
+    content: `item ${k + offset}`,
+  }))
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
+}
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source)
+  const destClone = Array.from(destination)
+  const [removed] = sourceClone.splice(droppableSource.index, 1)
+
+  destClone.splice(droppableDestination.index, 0, removed)
+
+  const result = {}
+  result[droppableSource.droppableId] = sourceClone
+  result[droppableDestination.droppableId] = destClone
+
+  return result
+}
+const grid = 8
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+})
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250,
+})
 
 const TaskList = (props) => {
   const list = props.list
@@ -18,6 +70,33 @@ const TaskList = (props) => {
   const toDoFuncs = useContext(LocalStorageFuncs)
   const toastFuncs = useContext(ToastFuncs)
   const dialogFuncs = useContext(DialogFuncs)
+
+  const [state, setState] = useState([getItems(5)])
+
+  function onDragEnd(result) {
+    const { source, destination } = result
+
+    // dropped outside the list
+    if (!destination) {
+      return
+    }
+    const sInd = +source.droppableId
+    const dInd = +destination.droppableId
+
+    if (sInd === dInd) {
+      const items = reorder(state[sInd], source.index, destination.index)
+      const newState = [...state]
+      newState[sInd] = items
+      setState(newState)
+    } else {
+      const result = move(state[sInd], state[dInd], source, destination)
+      const newState = [...state]
+      newState[sInd] = result[sInd]
+      newState[dInd] = result[dInd]
+
+      setState(newState.filter((group) => group.length))
+    }
+  }
 
   const deleteItem = (e) => {
     e.preventDefault()
@@ -60,20 +139,46 @@ const TaskList = (props) => {
   }, [focusInfo])
 
   return (
-    <ul className="m-4 text-center">
-      {list
-        .filter((item) => item.isComplete === type.isComplete)
-        .map((item) => (
-          <TaskItem
-            key={item.id}
-            item={item}
-            setFocus={setFocus}
-            type={type}
-            changeIsCompleted={changeIsCompleted}
-            deleteItem={deleteItem}
-          />
-        ))}
-    </ul>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <ul className="m-4 text-center">
+        {list
+          .filter((item) => item.isComplete === type.isComplete)
+          .map((item, ind) => (
+            <Droppable key="droppable" droppableId="1">
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}
+                  {...provided.droppableProps}
+                >
+                  <Draggable key={item.id} draggableId={item.id} index={ind}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        <TaskItem
+                          key={item.id}
+                          item={item}
+                          setFocus={setFocus}
+                          type={type}
+                          changeIsCompleted={changeIsCompleted}
+                          deleteItem={deleteItem}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                </div>
+              )}
+            </Droppable>
+          ))}
+      </ul>
+    </DragDropContext>
   )
 }
 
